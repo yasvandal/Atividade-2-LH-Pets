374de2c229588b8643278d7f5476296cf7b1c2d5
Yasvadal <yasvandal@gmail.com>
2024-03-22 14:22:07 -0300

    versão 1.0

---
+++ Program.cs
@@ -0,0 +1,26 @@
+namespace Projeto_Web_Lh_Pets_Alunos;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Projeto Web - LH Pets versão 1");
+
+        app.UseStaticFiles();
+        app.MapGet("/index", (HttpContext context)=>{
+            context.Response.Redirect("index.html", false);
+      
+      });
+
+       Banco dba=new Banco();
+       app.MapGet("/listaClientes", (HttpContext context) =>{
+
+          context.Response.WriteAsync( dba.GetListaString());
+       });
+
+        app.Run();
+    }
+}
